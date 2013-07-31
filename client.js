096f2f7d51211567494d66382c7eeab294b8ce06
Ondrej Zara <ondrej.zara@firma.seznam.cz>
2013-07-31 13:40:49 +0200

    first version

---
+++ client.js
@@ -0,0 +1,37 @@
+var ZombieReporter = function() {
+	this._passed = 0;
+	this._failed = 0;
+
+	var scripts = document.querySelectorAll("script");
+	var last = scripts[scripts.length-1];
+	this._reportUrl = last.src.split("/").slice(0, -1).join("/") + "/report";
+}
+
+ZombieReporter.prototype.jasmineDone = function() {
+	var data = [];
+	data.push("passed=" + this._passed);
+	data.push("failed=" + this._failed);
+
+	var script = document.createElement("script");
+	script.src = this._reportUrl + "?" + data.join("&") + "&" + Math.random();
+	document.body.appendChild(script);
+}
+
+ZombieReporter.prototype.specDone = function(spec) {
+	switch (spec.status) {
+		case "failed":
+			this._failed++;
+		break;
+
+		case "passed":
+			this._passed++;
+		break;
+	}
+}
+
+ZombieReporter.prototype.jasmineStarted = function() {}
+ZombieReporter.prototype.suiteStarted = function() {}
+ZombieReporter.prototype.suiteDone = function() {}
+ZombieReporter.prototype.specStarted = function() {}
+
+jasmine.getEnv().addReporter(new ZombieReporter());
